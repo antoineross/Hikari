7b9337ef026ac2d55de9a5698cf3858755ff95b6
Antoine Ross <developer.antoine@gmail.com>
2024-08-06 23:53:54 +0800

    testimonials

--- config/testimonials.ts
+++ config/testimonials.ts
@@ -3,119 +3,84 @@ export const testimonials = [
     name: 'dcodesdev',
     title: 'TypeScript Developer',
     avatarFallback: 'DC',
-    avatarImg: '/path/to/avatar1.png',
+    avatarImg: '/images/dcodes.png',
     text: "That's beautiful bro!"
   },
   {
     name: 'SuhailKakar',
     title: 'Developer at joinOnboard',
     avatarFallback: 'SK',
-    avatarImg: '/path/to/avatar2.png',
+    avatarImg: '/images/SuhailKakar.jpg',
     text: "If you've built this a few months ago, it would have saved me hours :D"
   },
   {
     name: 'SaidAitmbarek',
     title: 'Founder of microlaunch.net',
     avatarFallback: 'SA',
-    avatarImg: '/path/to/avatar3.png',
+    avatarImg: '/images/said.jpg',
     text: "So cool, looks really clean. Any plan to open source it? ☺️ Wanna play with it!"
   },
   {
     name: 'magicuidesign',
     title: 'UI Design Company',
     avatarFallback: 'MU',
-    avatarImg: '/path/to/avatar4.png',
+    avatarImg: '/images/magicui.jpg',
     text: "Clean 🤌"
   },
   {
     name: 'YasmeenRoumie',
     title: 'Developer',
     avatarFallback: 'YR',
-    avatarImg: '/path/to/avatar5.png',
+    avatarImg: '/images/yasmeen.jpg',
     text: "Ooh would love to try this out"
   },
   {
     name: 'shadcn',
     title: 'Developer',
     avatarFallback: 'SC',
-    avatarImg: '/path/to/avatar6.png',
+    avatarImg: '/images/shadcn.jpg',
     text: "👀"
   },
   {
     name: 'bzagrodzki',
     title: 'Developer',
     avatarFallback: 'BZ',
-    avatarImg: '/path/to/avatar7.png',
+    avatarImg: '/images/bzrag.jpg',
     text: "Nice one! But I would prefer some more \"sans\" font 😉"
   },
   {
     name: 'MPlegas',
     title: 'Developer',
     avatarFallback: 'MP',
-    avatarImg: '/path/to/avatar8.png',
+    avatarImg: '/images/MPlegas.jpg',
     text: "Exceptional!"
-  },
-  {
-    name: 'Muhammet_Atas_',
-    title: 'Developer',
-    avatarFallback: 'MA',
-    avatarImg: '/path/to/avatar9.png',
-    text: "I don't like light theme but this one look really clean and cool congrats👏🏻"
   },
   {
     name: 'kvncyf_',
     title: 'Developer',
     avatarFallback: 'KC',
-    avatarImg: 'https://x.com/MPlegas/photo',
-    text: "Nice move. wanted to use it few months ago but was outdated in many ways"
+    avatarImg: '/images/kvn.jpg',
+    text: "Nice move."
   },
   {
     name: '0xRaduan',
     title: 'Developer',
     avatarFallback: 'RA',
-    avatarImg: '/path/to/avatar11.png',
-    text: "This looks fire - where can I give a star / follow the development?"
+    avatarImg: '/images/0xraduan.jpg',
+    text: "This looks fire."
   },
   {
     name: 'Luax0',
     title: 'Developer',
     avatarFallback: 'LX',
-    avatarImg: '/path/to/avatar12.png',
+    avatarImg: '/images/luax0.jpg',
     text: "Can't wait to see more 👀"
-  },
-  {
-    name: '0xStanftf',
-    title: 'Developer',
-    avatarFallback: 'SF',
-    avatarImg: '/path/to/avatar13.png',
-    text: "Any GitHub repo Legend"
-  },
-  {
-    name: 'dew_yashtwt',
-    title: 'Developer',
-    avatarFallback: 'YD',
-    avatarImg: '/path/to/avatar14.png',
-    text: "Repo link?"
-  },
-  {
-    name: 'sanjay_4O4',
-    title: 'Developer',
-    avatarFallback: 'SK',
-    avatarImg: '/path/to/avatar15.png',
-    text: "Looks cool, throw auth using lucia and email using resend and it would be the perfect boilerplate"
   },
   {
     name: 'ausrobdev',
     title: 'Developer',
     avatarFallback: 'RA',
-    avatarImg: '/path/to/avatar16.png',
+    avatarImg: '/images/robdev.jpg',
     text: "Let me know when its ready, I'll add it to buildatlightspeed.com - we need more high quality open source boilerplates"
-  },
-  {
-    name: 'DewanganSajal',
-    title: 'Developer',
-    avatarFallback: 'SD',
-    avatarImg: '/path/to/avatar17.png',
-    text: "Sir, If you could integrate stripe into this, then I am sold 🫡"
   }
 ];
