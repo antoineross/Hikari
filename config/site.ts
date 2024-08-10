2f10fca60517b7a5d2f6a2f8bf83bfe1e7be6789
Antoine Ross <126068928+antoineross@users.noreply.github.com>
2024-08-10 23:37:26 +0800

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -3,11 +3,11 @@ import { SiteConfig } from 'types';
 export const siteConfig: SiteConfig = {
   name: 'Hikari',
   description:
-    'A complete & open-source Next.js 14 SaaS template using Supabase.',
-  url: 'https://tx.shadcn.com',
-  ogImage: 'https://tx.shadcn.com/og.jpg',
+    'A complete & open-source Next.js 14 Subscription Starter template using Supabase, Stripe, Tailwind CSS.',
+  url: 'https://hikari.antoineross.com',
+  ogImage: 'https://hikari.antoineross.com/og.jpg',
   links: {
     twitter: 'https://twitter.com/antoineross__',
-    github: 'https://github.com/antoineross/complete-nextjs-saas-starter'
+    github: 'https://github.com/antoineross/hikari'
   }
 };
