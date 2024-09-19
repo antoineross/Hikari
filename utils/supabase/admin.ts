b4aa91d9eaeab8aadf2711c678e336a3f7fccfb2
Antoine Ross <developer.antoine@gmail.com>
2024-09-19 11:05:25 +0800

    adding tRPC

--- utils/supabase/admin.ts
+++ utils/supabase/admin.ts
@@ -51,7 +51,9 @@ const upsertPriceRecord = async (
     unit_amount: price.unit_amount ?? null,
     interval: price.recurring?.interval ?? null,
     interval_count: price.recurring?.interval_count ?? null,
-    trial_period_days: price.recurring?.trial_period_days ?? TRIAL_PERIOD_DAYS
+    trial_period_days: price.recurring?.trial_period_days ?? TRIAL_PERIOD_DAYS,
+    description: null,
+    metadata: null
   };
 
   const { error: upsertError } = await supabase
